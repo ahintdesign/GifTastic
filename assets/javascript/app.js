99292754c040a926a16f5ada44463a7fa4f6b14a
Apple User <appleuser@mac-c82a14441071.attlocal.net>
2017-10-04 21:45:43 -0400

    Working files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@
       function displayEmotionGifs() {
 
         var emotion = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + emotion + "&api_key=zmzElrIeWk0nQB8yTpab25TMDXGDR1M5";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + emotion + "&api_key=zmzElrIeWk0nQB8yTpab25TMDXGDR1M5&limit=10";
 
 
 
@@ -21,25 +21,50 @@
 
 
           var showEmotions = response.data[i].images.fixed_height.url;
+          var rating = response.data[i].rating;
           var still = response.data[i].images.fixed_height_still.url;
           var animate = response.data[i].images.fixed_height.url;
+        
 
           var image = $("<img>");
           image.attr("src", showEmotions);
-          //image.attr("data-state" , still);
-          //image.attr("class" , gif);
+          image.attr("data-state" , still);
+          image.attr("data-still" , still);
+          image.attr("data-animate" , animate);
+          image.addClass("gif");
+          
+           //TESTING
+           // console.log(image);
 
+          var pOne = $("<p>");
+          pOne.append(rating);
 
-          console.log(image);
-          //var pOne = $("<p>").append(image)
-
-          //TESTING
+       
 
          gifDiv.append(image);
-
-          
+         gifDiv.append(pOne);
           $("#images").prepend(gifDiv);
+
       }
+
+
+     $(".gif").on("click", function() {
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
+
+
+
         });
 
       }
@@ -83,6 +108,7 @@
         
         showButtons();
       });
+
 
       
       $(document).on("click", ".buttonFeeling", displayEmotionGifs);
